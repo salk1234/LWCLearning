4bee71fe54a019c6cf40bcd06794807e5e984b3f
Thulluri Khyathi <khyathithulluri1999@gmail.com>
2025-12-09 07:58:20 +0530

    Story/LWC-002 publish product selection

--- force-app/main/default/lwc/opportunityWizard/opportunityWizard.js
+++ force-app/main/default/lwc/opportunityWizard/opportunityWizard.js
@@ -1,6 +1,8 @@
 import { LightningElement,wire,api } from 'lwc';
 
 export default class OpportunityWizard extends LightningElement {
+    prdCount=0;
+
     displayOppoPage=true;;
     oppoName;
     selectAcntId;
@@ -10,20 +12,32 @@ export default class OpportunityWizard extends LightningElement {
     stepDetails="1";
     errorValidation=false;
     displayProdPage=false;
+    selectedProdIds;
+   
     
-    /*handleClick() {
-        this.stepDetails ='1'
-        this.displayOppoPage=true;    
-        console.log('this.modal>>',this.displayOppoPage);    
-    }*/
+ 
     get disableNext(){
-        console.log('In disable next>>',this.selectAcntId,this.closeDate,this.Stage,this.Amount,this.oppoName);
-        return !(this.selectAcntId && this.closeDate && this.Stage && this.Amount && this.oppoName);
+        if(this.stepDetails==="1"){
+            return !(this.selectAcntId && this.closeDate && this.Stage && this.Amount && this.oppoName);  
+        }
+        if(this.stepDetails==="2"){
+            /* const child = this.template.querySelector('c-product-selection');
+            this.prdCount = child?.count??0; */
+            return this.prdCount == 0;
+            // return this.template.querySelector('c-product-selection')?.count??0;
+        }
+        if(this.stepDetails==="3"){
+        }
     }
     nextPage1(){
        this.displayOppoPage=false;
        this.stepDetails="2";
        this.displayProdPage=true;
+       if(this.stepDetails==="2" && this.prdCount>0 ){
+            this.stepDetails="3";
+           this.displayProdPage=false;
+           this.displaySelectedProd=true;
+       }
     } 
     handleChange(event){
         console.log('Event>',event.detail);
@@ -32,5 +46,9 @@ export default class OpportunityWizard extends LightningElement {
         this.Amount = event.detail.amount;
         this.oppoName = event.detail.oppoName;
         this.selectAcntId = event.detail.selectedAcntId;
+    }
+    updatePrdCnt(event){
+        this.prdCount = event.detail.count;
+        this.selectedProdIds = event.detail.selectedRows;
     }
 }   
